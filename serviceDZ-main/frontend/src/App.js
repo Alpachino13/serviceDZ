12a55c443b4facd7aaaaf01c1103b4c5456d8cc8
Alpachino13 <merabetzakariamehdi@gmail.com>
2026-04-27 19:40:02 +0100

    Ajout des fichiers frontend et config IP serveur

---
+++ serviceDZ-main/frontend/src/App.js
@@ -0,0 +1,11 @@
+import ServiceDZHome from './components/ServiceDZHome';
+
+function App() {
+  return (
+    <div className="App">
+      <ServiceDZHome />
+    </div>
+  );
+}
+
+export default App;
